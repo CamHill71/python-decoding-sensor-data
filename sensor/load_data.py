a11f41bb4149024a9d25fe4fd4d6c45a6104b4b6
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-17 14:22:01 +0800

    s

---
+++ sensor/load_data.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 17 13:38:14 2022
+
+@author: tafe2
+"""
+
+import os
+import glob
+import csv
+
+
+def load_sensor_data():
+    sensor_data = []
+    sensor_files = glob.glob(os.path.join(os.getcwd(),"datasets","*.csv"))
+
+    for sensor_file in sensor_files:
+        with open(sensor_file) as data_file:
+            data_reader = csv.DictReader(data_file,delimiter=',') 
+            for row in data_file:
+                sensor_data.append(row)
+
+
+    return sensor_data
